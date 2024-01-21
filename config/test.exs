6e3894b864bacb5c534254748f556d03e9200f37
Aaron Seigo <aseigo@kde.org>
2024-01-21 16:39:45 +0100

    use dotenvy for the db url in tests as well

--- config/test.exs
+++ config/test.exs
@@ -9,10 +9,6 @@ config :bcrypt_elixir, :log_rounds, 1
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :cklist, Cklist.Repo,
-  username: "cklist",
-  password: "cklist",
-  hostname: "localhost",
-  database: "cklist_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: 10
 
