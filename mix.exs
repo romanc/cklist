acd1275cb96220e7f59343042c6f03b7df083ff9
Aaron Seigo <aseigo@kde.org>
2024-01-21 15:06:43 +0100

    include the envs overlay in releases

--- mix.exs
+++ mix.exs
@@ -9,7 +9,10 @@ defmodule Cklist.MixProject do
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      releases: [
+        overlays: ["envs/"]
+      ]
     ]
   end
 
