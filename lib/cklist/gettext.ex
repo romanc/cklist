12c5a69c0d4c5b8b5becd2447c3378e4d6e67124
Aaron Seigo <aseigo@kde.org>
2024-01-23 19:21:12 +0100

    Put the Gettext module in `lib/cklist/`

--- lib/cklist/gettext.ex
+++ lib/cklist/gettext.ex
@@ -1,11 +1,11 @@
-defmodule CklistWeb.Gettext do
+defmodule Cklist.Gettext do
   @moduledoc """
   A module providing Internationalization with a gettext-based API.
 
   By using [Gettext](https://hexdocs.pm/gettext),
   your module gains a set of macros for translations, for example:
 
-      import CklistWeb.Gettext
+      import Cklist.Gettext
 
       # Simple translation
       gettext("Here is the string to translate")
