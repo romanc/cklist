2a96f582f790d62802df40b91560191d2447d199
Roman Cattaneo <>
2024-02-26 00:00:39 +0100

    Simple UI for sequential chcklists

--- lib/cklist_web/live/cklist_run_live.ex
+++ lib/cklist_web/live/cklist_run_live.ex
@@ -15,11 +15,10 @@ defmodule CklistWeb.CklistRunLive do
       |> assign(:steps_done, 0)
       |> assign(:completed, false)
       |> assign(:step_state, %{})
-    {:ok, socket}
-  end
 
-  def handle_event("completed", _params, socket) do
-    { :noreply, assign(socket, :completed, true) }
+      |> assign(:current_step, Enum.at(checklist.document["steps"], 0))
+      |> assign(:next_step, Enum.at(checklist.document["steps"], 1))
+    {:ok, socket}
   end
 
   def handle_event("step_done", params, %{assigns: assigns} = socket) do
@@ -32,6 +31,42 @@ defmodule CklistWeb.CklistRunLive do
       |> assign(:step_state, updated_state)
       |> assign(:steps_done, Enum.reduce(updated_state,  0, &is_done/2))
     }
+  end
+
+  def handle_event("completed", _params, socket) do
+    { :noreply, assign(socket, :completed, true) }
+  end
+
+  def handle_event("abort", _params, %{assigns: assigns} = socket) do
+    {
+      :noreply,
+      socket
+        |> put_flash(:info, "Aborting checklist run")
+        |> redirect(to: ~p"/checklists/#{assigns.checklist}")
+    }
+  end
+
+  def handle_event("next_step", _params, %{assigns: assigns} = socket) do
+    case assigns.completed do
+      true ->
+        {
+        :noreply,
+        socket
+          |> put_flash(:info, "Well done!")
+          |> redirect(to: ~p"/checklists/#{assigns.checklist}")
+        }
+      false ->
+        updated_steps_done = assigns.steps_done + 1
+        completed = assigns.steps === updated_steps_done
+        {
+        :noreply,
+        socket
+          |> assign(:steps_done, updated_steps_done)
+          |> assign(:current_step, Enum.at(assigns.checklist.document["steps"], updated_steps_done))
+          |> assign(:next_step, Enum.at(assigns.checklist.document["steps"], updated_steps_done + 1))
+          |> assign(:completed, completed)
+        }
+    end
   end
 
   defp is_done({_, true}, count), do: count + 1
