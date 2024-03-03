70edb66eb70e228919ff30d9f1c8d9c2d9814844
Roman Cattaneo <>
2024-03-03 13:26:59 +0100

    Move buttons to re-useable components

--- lib/cklist_web/components/my_components.ex
+++ lib/cklist_web/components/my_components.ex
@@ -22,4 +22,61 @@ defmodule CklistWeb.MyComponents do
     </div>
     """
   end
+
+  @doc """
+  Renders an abort button.
+
+  ## Example
+
+    <.abort_button hidden={@completed} />
+  """
+  attr :class, :string, default: nil
+  attr :hidden, :boolean, default: false
+
+  def abort_button(assigns) do
+    ~H"""
+    <.button
+      class={"bg-gray-300 hover:bg-gray-400 #{if @hidden, do: "hidden", else: ""} #{@class}"}
+      phx-click="abort"
+      data-confirm="Are you sure?"
+    >
+      Abort
+    </.button>
+    """
+  end
+
+  @doc """
+  Renders a button to go to the next step.
+
+  ## Example
+
+    <.next_button hidden={not @completed}>
+      Awesome!
+    </.next_button>
+  """
+  attr :class, :string, default: nil
+  attr :hidden, :boolean, default: false
+  slot :inner_block, default: "Next"
+
+  def next_button(assigns) do
+    ~H"""
+    <.button
+      class={"bg-lime-500 hover:bg-lime-400 #{if @hidden, do: "hidden", else: ""} #{@class}"}
+      phx-click="next_step"
+    >
+      <%= render_slot(@inner_block) %>
+    </.button>
+    """
+  end
+
+  attr :class, :string, default: nil
+  attr :rest, :global, doc: "arbitrary HTML attributes to apply to the button tag"
+  slot :inner_block, required: true
+  defp button(assigns) do
+    ~H"""
+    <button class={"py-2 rounded-lg #{@class}"} {@rest}>
+      <%= render_slot(@inner_block) %>
+    </button>
+    """
+  end
 end
