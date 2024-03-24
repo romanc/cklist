6bc8fade31436d7faf10f8d092876c03dc884a08
Roman Cattaneo <>
2024-03-24 16:46:23 +0100

    WIP live updating checklist steps

--- lib/cklist_web/live/cklist_new_live.ex
+++ lib/cklist_web/live/cklist_new_live.ex
@@ -9,9 +9,39 @@ defmodule CklistWeb.CklistNewLive do
   end
 
   def mount(_params, _session, socket) do
-    changeset = Checklists.change_checklist(%Checklist{})
+    changeset = Checklists.change_checklist(
+      %Checklist{},
+      %{ document: %{ sequential: true, steps: [%{name: "one"}, %{name: "two"}] } }
+    )
+
+    IO.inspect(changeset)
+
     socket = socket
       |> assign(:changeset, changeset)
     {:ok, socket}
+  end
+
+  # leave text field
+  def handle_event("new_step", params, socket) do
+    document = socket.assigns.changeset.changes.document
+
+    IO.inspect(socket.assigns.changeset)
+
+    {:ok, changeset} = Checklists.update_checklist(
+      socket.assigns.changeset,
+      %{ document: %{
+        sequential: document.sequential,
+        steps: [document.steps | %{name: params["new_step"]}]
+      }}
+    )
+
+    socket = socket
+      |> assign(:changeset, changeset)
+    { :noreply, socket }
+  end
+
+  def handle_event("step_changed", params, socket) do
+    IO.inspect(params)
+    { :noreply, socket }
   end
 end
