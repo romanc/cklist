55c9905b63af47778af1f568694493217ab52180
Roman Cattaneo <>
2025-05-26 23:16:30 +0200

    ci: fully automated releases

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Cklist.MixProject do
   def project do
     [
       app: :cklist,
-      version: "0.0.2",
+      version: "0.0.3",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
