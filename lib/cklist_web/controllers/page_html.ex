0ecace20223e6934ff88672a3dae6f2e0fe2498e
Roman Cattaneo <>
2025-06-22 10:12:10 +0200

    mix gen auth and first round of html templating

--- lib/cklist_web/controllers/page_html.ex
+++ lib/cklist_web/controllers/page_html.ex
@@ -7,4 +7,18 @@ defmodule CklistWeb.PageHTML do
   use CklistWeb, :html
 
   embed_templates "page_html/*"
+
+  attr :title, :string, required: true
+  attr :desc, :string, default: nil
+  def awesome(assigns) do
+    ~H"""
+    <div class="flex items-start my-4">
+      <img src={~p"/images/logo.svg"} width="30" />
+      <div class="ml-4">
+        <p class="text-lg text-zinc-600">{@title}</p>
+        <p :if={@desc} class="mt-2 text-sm">{@desc}</p>
+      </div>
+    </div>
+    """
+  end
 end
