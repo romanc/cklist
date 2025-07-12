cfecc9f8926be08343bba9d22cd0c5e9de179c70
Roman Cattaneo <>
2025-07-12 10:21:43 +0200

    Allow manual deployments for testing

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Cklist.MixProject do
   def project do
     [
       app: :cklist,
-      version: "0.0.6",
+      version: "0.0.7",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
