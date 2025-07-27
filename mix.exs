1cdb6d7fa9a29e57e3b2d4003ea510f1fa573ffd
Roman Cattaneo <>
2025-07-27 23:21:50 +0200

    this seems to work locally

--- mix.exs
+++ mix.exs
@@ -59,7 +59,8 @@ defmodule Cklist.MixProject do
       {:gettext, "~> 0.26"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:hackney, "~> 1.25"}
     ]
   end
 
