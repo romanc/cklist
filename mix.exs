e1e311c5119eb81317da6770873dd2abf9e014be
Roman Cattaneo <>
2025-08-10 17:33:50 +0200

    ci: Clean up deployments
    Things I learned during work on PR #34:
    
    - A dummy DATABASE_URL is sufficient to build the release
    - Run migrations as part of `ExecStartPre` in the service file
      definition. This allows to re-use the production env vars already
      configured in the service defintion file.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Cklist.MixProject do
   def project do
     [
       app: :cklist,
-      version: "0.0.9",
+      version: "0.0.10",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
