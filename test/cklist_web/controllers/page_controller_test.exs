dcda0e99cc8e5bfded31c7fb5f14a624e596c12a
Roman Cattaneo <>
2025-08-16 12:26:07 +0200

    Simple tests for about page

--- test/cklist_web/controllers/page_controller_test.exs
+++ test/cklist_web/controllers/page_controller_test.exs
@@ -5,4 +5,21 @@ defmodule CklistWeb.PageControllerTest do
     conn = get(conn, ~p"/")
     assert html_response(conn, 200) =~ "Check that list"
   end
+
+  describe "about.html" do
+    test "contains current app version", %{conn: conn} do
+      conn = get(conn, ~p"/about")
+      assert html_response(conn, 200) =~ "cklist version #{Application.spec(:cklist, :vsn)}"
+    end
+
+    test "contains phx version", %{conn: conn} do
+      conn = get(conn, ~p"/about")
+      assert html_response(conn, 200) =~ "phoenix #{Application.spec(:phoenix, :vsn)}"
+    end
+
+    test "renders faq", %{conn: conn} do
+      conn = get(conn, ~p"/about")
+      assert html_response(conn, 200) =~ "FAQ"
+    end
+  end
 end
